34c3748e6bc542de4470688ec8b11c9984284cc7
huusz <irinso93@gmail.com>
2021-06-25 20:43:35 +0900

    css module 적용
    issue: #3

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={styles.App}>
+      <header className={styles.header}>
+        <img src={logo} className={styles.logo} alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className={styles.link}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
